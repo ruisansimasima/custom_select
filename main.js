73b7ced8c8b96b3b26a7f04343630eae03a1e9be
jun <grandtheftautokawahara@gmail.com>
2023-10-09 23:49:21 +0900

    保存

--- main.js
+++ main.js
@@ -20,7 +20,15 @@ import { csvToJson } from './csv.js';
         let input = document.createElement('input');
         input.setAttribute("type","radio");
         input.setAttribute("name","top");
+        input.setAttribute("data-top-parts",setTop[i]);
+        input.setAttribute("id",setTop[i]);
+        input.setAttribute("class","radio_button");
         select01Zone[0].appendChild(input);
+
+        let label = document.createElement('label');
+        label.setAttribute("for",setTop[i]);
+        label.innerText = setTop[i];
+        select01Zone[0].appendChild(label);
     }
 
     let centerlength = [];
@@ -29,12 +37,19 @@ import { csvToJson } from './csv.js';
     });
     let setCenter = Array.from(new Set(centerlength));//重複を除いたcenter
     let select02Zone = document.getElementsByClassName('select02_zone');
-    console.log(setCenter);
     for(let i=0; i<setCenter.length; i++){
         let input = document.createElement('input');
         input.setAttribute("type","radio");
         input.setAttribute("name","center");
+        input.setAttribute("data-center-parts",setCenter[i]);
+        input.setAttribute("id",setCenter[i]);
+        input.setAttribute("class","radio_button");
         select02Zone[0].appendChild(input);
+
+        let label = document.createElement('label');
+        label.setAttribute("for",setCenter[i]);
+        label.innerText = setCenter[i];
+        select02Zone[0].appendChild(label);
     }
 
     let underlength = [];
@@ -43,12 +58,35 @@ import { csvToJson } from './csv.js';
     });
     let setUnder = Array.from(new Set(underlength));//重複を除いたunder
     let select03Zone = document.getElementsByClassName('select03_zone');
-    console.log(setUnder);
     for(let i=0; i<setUnder.length; i++){
         let input = document.createElement('input');
         input.setAttribute("type","radio");
         input.setAttribute("name","under");
+        input.setAttribute("data-under-parts",setUnder[i]);
+        input.setAttribute("id",setUnder[i]);
+        input.setAttribute("class","radio_button");
         select03Zone[0].appendChild(input);
+
+        let label = document.createElement('label');
+        label.setAttribute("for",setUnder[i]);
+        label.innerText = setUnder[i];
+        select03Zone[0].appendChild(label);
     }
 
+    changeImage();
 })();
+
+//メイン画像変更処理
+function changeImage(){
+    console.log("aaaaaa");
+}
+
+
+window.addEventListener('load',()=>{
+    //読み込み時のボタンの初期値
+    let selectZone = document.querySelectorAll(".select_zone");
+    for(let i=0;i<selectZone.length;i++){
+        let inputRadio = selectZone[i].querySelectorAll(".radio_button");
+        inputRadio[0].checked = true;
+    }
+},false);
